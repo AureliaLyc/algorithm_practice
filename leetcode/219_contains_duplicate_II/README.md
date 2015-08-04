aaa5cdb9ed8f525817bfaa10df42904318c98439
newlifeyh <newlifeyh@gmail.com>
2015-08-04 19:27:00 +0800

    for leetcode contains duplicate II, unordered_set method

--- leetcode/219_contains_duplicate_II/README.md
+++ leetcode/219_contains_duplicate_II/README.md
@@ -3,8 +3,4 @@ Contains Duplicate II
 Given an array of integers and an integer *k*, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
 
 ==
-这个问题是说，给定一个数组，和一个k值，看看在序号差小于等于k这些组合里面，有没有相等的情况出现。
-既然要寻找，那就要先排序？具有消除的特性，检查完一个，就可以把它删掉。
-我要维护一个K－1长的数组，始终让其保持排序状态。
-* 先判断当前排序后的K-1数组中有没有当前数值，
-* 没有，就binarysearch下一个数，并把它替换成下一个数，然后对他进行排序。
+想了好半天，最终明白，什么标准库都可以用啊，尤其是hashset什么的。现在的方法是，维持一个unorderedSet，保证它的size小于k，不停的往里面插入当前值，如果插入不成功，证明有重复的，那就返回true。
--- leetcode/219_contains_duplicate_II/main.cc
+++ leetcode/219_contains_duplicate_II/main.cc
@@ -1,31 +1,20 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        first=0;
-        for (std::vector<int>::iterator i = nums.begin(); i != nums.end(); ++i)
+        unordered_set<int> mySet;
+        int first = 0;
+        for (int i = 0; i < nums.size(); ++i)
         {
-            int val = *i;
-            bool res = insertToPool( val, k, nums );
-            if (!res)
+            int val = nums[i];
+            if (mySet.size() > k)
+            {
+                mySet.erase(nums[first++]);
+            }
+            if (!mySet.insert(val).second)
             {
                 return true;
             }
         }
         return false;
     }
-    bool insertToPool(int val, int k, std::vector<int>& nums){
-        if (myPool.size() > k)
-        {
-            pop(nums[first]);
-            first++;
-        }
-        bool res = myPool.insert( val ).second;
-        return res;
-    }
-    void pop( int val ){
-        myPool.erase(val);
-    }
-private:
-    set<int> myPool;
-    int first;
 };
