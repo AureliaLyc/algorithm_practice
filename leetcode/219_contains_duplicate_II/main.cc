51b9e7f4c5f152280081c22436802c36928c237b
newlifeyh <newlifeyh@gmail.com>
2015-08-04 18:48:25 +0800

    for leet code, use set instead of vector sort. performane is better

--- leetcode/219_contains_duplicate_II/main.cc
+++ leetcode/219_contains_duplicate_II/main.cc
@@ -1,54 +1,31 @@
-#include<vector>
-using namespace std;
-
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        if(nums.empty() || k < 1)
-        return false;
-
-        initVector(nums, k);
-        for (int i = 0; i < nums.size(); ++i)
+        first=0;
+        for (std::vector<int>::iterator i = nums.begin(); i != nums.end(); ++i)
         {
-            int val = nums[i];
-            bool fnd = binary_search(vCan.begin(), vCan.end(), val);
-            if (fnd)
+            int val = *i;
+            bool res = insertToPool( val, k, nums );
+            if (!res)
             {
                 return true;
             }
-            else{
-                if ( i+1 < nums.size() )
-                {
-                    int nextVal = nums[i+1];
-                    std::vector<int>::iterator it = lower_bound(vCan.begin(), vCan.end(), nextVal);
-                    vCan.erase(it);
-                }
-                if ( i+1+k < nums.size() )
-                {
-                    int nextVal = nums[i+1+k];
-                    vCan.push_back(nextVal);
-                    sort(vCan.begin(), vCan.end());
-                }
-            }
         }
         return false;
     }
-    void initVector( std::vector<int>& nums, int k ){
-        for (int i = 1; i < k+1 && i < nums.size(); ++i)
+    bool insertToPool(int val, int k, std::vector<int>& nums){
+        if (myPool.size() > k)
         {
-            vCan.push_back( nums[i] );
+            pop(nums[first]);
+            first++;
         }
-        sort(vCan.begin(), vCan.end());
+        bool res = myPool.insert( val ).second;
+        return res;
+    }
+    void pop( int val ){
+        myPool.erase(val);
     }
 private:
-    std::vector<int> vCan;
+    set<int> myPool;
+    int first;
 };
-
-int main(int argc, char const *argv[])
-{
-    Solution sol1;
-    int abc[] = {1,2,1};
-    std::vector<int> v(abc, abc+3);
-    bool res = sol1.containsNearbyDuplicate(v, 1);
-    return 0;
-}
