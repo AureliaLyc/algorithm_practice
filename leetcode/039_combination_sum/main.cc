fe4826d6ec81a9b0a19a9fd8e80b14a523a03540
newlifeyh <newlifeyh@gmail.com>
2015-09-07 18:10:32 +0800

    for leetcode combination sum

--- leetcode/039_combination_sum/main.cc
+++ leetcode/039_combination_sum/main.cc
@@ -1,36 +1,21 @@
 class Solution {
 public:
     void getSum(vector<int>& candidates, int target, int left, vector<int>& v){
-        if(candidates[left]<=target){
-            v.push_back(candidates[left]);
-            target -= candidates[left];
-            if(target == 0){
-                combSet.push_back(v);
+        if(target == 0){
+            combSet.push_back(v);
+        }
+        else {
+            for(; left<candidates.size() && target >= candidates[left]; ++left){
+                v.push_back(candidates[left]);
+                getSum(candidates, target-candidates[left], left, v);
+                v.pop_back();
             }
-            else if (target >= candidates[left]){
-                int start = left;
-                int right = candidates.size();
-                // try every
-                while(start<right && candidates[start]<=target){
-                    getSum(candidates, target, start, v);
-                    ++start;
-                }
-            }
-            v.pop_back();
         }
     }
     vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
-        int size = candidates.size();
-        int left = 0;
         sort(candidates.begin(), candidates.end());
-        while(left<size && candidates[left]<=target){
-            if(left != 0){
-                while(left<size && candidates[left] == candidates[left-1]) ++left;
-            }
-            vector<int> v;
-            getSum(candidates, target, left, v);
-            ++left;
-        }
+        vector<int> v;
+        getSum(candidates, target, 0, v);
         return combSet;
     }
     vector<vector<int> > combSet;
