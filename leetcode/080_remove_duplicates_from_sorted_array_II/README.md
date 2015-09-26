21f8b524e81e4b5651abee49c9ebe79d37634134
newlifeyh <newlifeyh@gmail.com>
2015-09-26 23:41:48 +0800

    for leetcode remove duplicates from sorted array II

--- leetcode/080_remove_duplicates_from_sorted_array_II/README.md
+++ leetcode/080_remove_duplicates_from_sorted_array_II/README.md
@@ -1,2 +1,12 @@
 # Remove Duplicates from Sorted Array II
 ==
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
+
+## 理解
+首先是排序的数组，回头看几个数字，如果相等，意味着中间的数都是相等的。
--- leetcode/080_remove_duplicates_from_sorted_array_II/main.cc
+++ leetcode/080_remove_duplicates_from_sorted_array_II/main.cc
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.empty()){
-            return 0;
-        }
         int size=nums.size();
-        int left=1, right=2;
+        int left=2, right=2;
         while(right<size){
-            if(nums[left]!=nums[right] || nums[left-1]!=nums[right])
+            if(nums[left-2]!=nums[right])
             {
-                ++left;
-                nums[left]=nums[right];
+                nums[left++]=nums[right];
             }
             ++right;
         }
-        return left<size?left+1:size;
+        return left<size?left:size;
     }
 };
