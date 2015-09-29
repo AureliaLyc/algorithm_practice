28deb793f81d524913031b796ec16513629fdced
newlifeyh <newlifeyh@gmail.com>
2015-09-29 11:00:22 +0800

    for leetcode remove duplicates from sorted list II

--- leetcode/082_remove_duplicates_from_sorted_list_II/main.cc
+++ leetcode/082_remove_duplicates_from_sorted_list_II/main.cc
@@ -42,3 +42,33 @@ public:
         return res?res:head;
     }
 };
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* res=NULL;
+        ListNode* tail=NULL;
+        ListNode* right=head->next;
+        while(head){
+            while(right && head->val==right->val){
+                right=right->next;
+            }
+            if(head->next == right){
+                if(res==NULL){
+                    res=head;
+                }
+                else{
+                    tail->next=head;
+                }
+                tail=head;
+                tail->next=NULL;
+            }
+            head=right;
+            right=right?right->next:NULL;
+        }
+        return res;
+    }
+};
