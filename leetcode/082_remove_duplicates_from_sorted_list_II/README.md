1b6ecfbb04be3d41c236e8dde20f1c89f2cf4b8d
newlifeyh <newlifeyh@gmail.com>
2015-09-29 09:57:15 +0800

    for leetcode remove duplicates from sorted list II

---
+++ leetcode/082_remove_duplicates_from_sorted_list_II/README.md
@@ -0,0 +1,15 @@
+# Remove Duplicates from Sorted List II
+==
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+```
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+```
+
+## 理解
+ * 主要是针对最开始遇到重复，怎么处理这四个指针
+ * 前进后，两种情况又怎么处理
+
+所以这是一个考察想的是否全面的一个问题
---
+++ leetcode/082_remove_duplicates_from_sorted_list_II/main.cc
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* res=NULL;
+        ListNode* left=head;
+        ListNode* right=left->next;
+        while(right){
+            while(right && left->val==right->val){
+                right=right->next;
+            }
+            if(left->next != right){
+                if(head==left){
+                    head=right;
+                    left=right;
+                }else{
+                    head->next=right;
+                    left=right;
+                }
+            }
+            else if(res==NULL){
+                res=left;
+                head=left;
+                left=left->next;
+            }
+            else{
+                head=left;
+                left=left->next;
+            }
+            right=right?right->next:NULL;
+        }
+        return res?res:head;
+    }
+};
