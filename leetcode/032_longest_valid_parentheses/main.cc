f5a8fb174afb2af0d178b09f958c07e35653478a
Dayong Li <dayong@mac-dayong.local>
2016-02-19 11:07:46 +0800

    for leetcode longest valid parentheses

--- leetcode/032_longest_valid_parentheses/main.cc
+++ leetcode/032_longest_valid_parentheses/main.cc
@@ -20,3 +20,25 @@ public:
         return res;
     }
 };
+
+class Solution2 {
+public:
+    int longestValidParentheses(string s) {
+        int size=s.length();
+        int res=0;
+        vector<int> d(size, 0);
+        for(int i=1; i<size; ++i){
+            if(s[i]==')'){
+                int prev=i-1-d[i-1];
+                if(prev>=0 && s[prev]=='('){
+                    d[i]=d[i-1]+2;
+                    if(prev>1){
+                        d[i]=d[i]+d[prev-1];
+                    }
+                }
+                res=max(res, d[i]);
+            }
+        }
+        return res;
+    }
+};
