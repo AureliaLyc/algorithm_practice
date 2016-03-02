a8019c7c67783bcec76a8bb54a6f95f344d3787c
ClubMaster <newlifehy@gmail.com>
2016-03-02 21:50:00 +0800

    for leetcode second round

--- leetcode/096_unique_binary_search_trees/main.cc
+++ leetcode/096_unique_binary_search_trees/main.cc
@@ -18,3 +18,19 @@ public:
         return calculate(1, n, cache);
     }
 };
+
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n==0) return 0;
+        vector<int> d(n+1, 0);
+        d[0]=1;
+        d[1]=1;
+        for(int i=2; i<=n; ++i){
+            for(int j=0; j<i; ++j){
+                d[i]+=d[j]*d[i-j-1];
+            }
+        }
+        return d.back();
+    }
+};
