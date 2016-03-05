ae9d17d75fafb79fdca965715a260b1c4cd39484
ClubMaster <newlifehy@gmail.com>
2016-03-05 11:05:05 +0800

    for leetcode second round

--- leetcode/106_construct_binary_tree_from_inorder_and_postorder_traversal/main.cc
+++ leetcode/106_construct_binary_tree_from_inorder_and_postorder_traversal/main.cc
@@ -29,3 +29,34 @@ public:
         return root;
     }
 };
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        TreeNode* root=nullptr;
+        if(postorder.empty()) return root;
+        root=new TreeNode(postorder[postorder.size()-1]);
+        stack<TreeNode*> stac;
+        stac.push(root);
+        int j=inorder.size()-1;
+        TreeNode* cur=nullptr;
+        for(int i=postorder.size()-2; i>=0; --i){
+            if(postorder[i+1]!=inorder[j]){
+                stac.top()->right=new TreeNode(postorder[i]);
+                stac.push(stac.top()->right);
+            }
+            else{
+                while(!stac.empty() && stac.top()->val==inorder[j]){
+                    cur=stac.top();
+                    stac.pop();
+                    --j;
+                }
+                if(j>=0){
+                    cur->left=new TreeNode(postorder[i]);
+                    stac.push(cur->left);
+                }
+            }
+        }
+        return root;
+    }
+};
