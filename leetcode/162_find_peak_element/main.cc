b02c4d6ee64b177b336b1a2344f2e8cf9a702206
ClubMaster <newlifehy@gmail.com>
2016-03-13 11:54:21 +0800

    leetcode second round

--- leetcode/162_find_peak_element/main.cc
+++ leetcode/162_find_peak_element/main.cc
@@ -13,3 +13,21 @@ public:
         return left;
     }
 };
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int size=nums.size();
+        int l=0, r=size-1;
+        while(l<r){
+            int m=(l+r)/2;
+            if(nums[m]<nums[m+1]){
+                l=m+1;
+            }
+            else{
+                r=m;
+            }
+        }
+        return l;
+    }
+};
